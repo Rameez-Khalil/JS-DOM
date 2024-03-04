9b91640643bcf4d16c6e55b8fd67bc1bc08c6df8
rameez <rameezkhalil23@gmail.com>
2024-03-04 03:36:43 -0800

    DOM introduction

---
+++ DOM/01 - DOM Methods/script.js
@@ -0,0 +1,5 @@
+//getElementById is a method.
+//innerHTML is a property, useful for getting or setting the content within the HTML element.
+
+const reference = (document.querySelector("#demo").innerHTML =
+  "Rameez is the greatest");
